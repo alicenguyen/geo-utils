09e92ae5c198744fa1a95c54ca248f245bba416b
Alice Bollong <amnguyen30@gmail.com>
2020-05-17 14:54:54 -0700

    adds progressbar

--- gene-parser.py
+++ gene-parser.py
@@ -3,33 +3,55 @@ import glob
 import gzip
 from collections import defaultdict
 
+import logging
+import progressbar
+
+progressbar.streams.wrap_stderr()
+logging.basicConfig(level=logging.INFO)
+
 DATA_FILEPATH = 'sample_data/*.txt.gz'
 TRANSCRIPT_ID_COL_IDX = 0
 TRANSCRIPT_ABUNDANCE_COL_IDX = 8
 
-overall_set_collection = defaultdict(list)
 sampled_transcripts_collection = defaultdict(dict)
-unique_sample_names = []
+unique_sample_names = set()
+
 
 def extract_sample_name(string):
     tokens = string.split('_')  # todo improve this
     return tokens[2] + '_' + tokens[3]
 
-for file_name in glob.glob(DATA_FILEPATH):
-    print 'processing ' + file_name
-    sample_name = extract_sample_name(file_name)
-    unique_sample_names.append(sample_name)
 
-    with gzip.open(file_name, 'rb') as in_file:
-        reader = csv.reader(in_file, delimiter='\t')
-        next(reader, None)  # skip  the header row
+sample_set_files = glob.glob(DATA_FILEPATH)
 
-        for line in [l for l in reader if l[1] != '-']:
-            gene_id = line[TRANSCRIPT_ID_COL_IDX]
-            abundance = line[TRANSCRIPT_ABUNDANCE_COL_IDX]
-            sampled_transcripts_collection[gene_id][sample_name] = abundance
+logging.info('Processing files..')
+with progressbar.ProgressBar(max_value=len(sample_set_files)) as bar:
+    for i in range(0, len(sample_set_files)):
+        file_name = sample_set_files[i]
 
-with open('test.csv', 'wb') as outfile:
+        sample_name = extract_sample_name(file_name)
+        unique_sample_names.add(sample_name)
+
+        with gzip.open(file_name, 'rb') as in_file:
+            reader = csv.reader(in_file, delimiter='\t')
+            next(reader, None)  # skip the header row
+
+            data = list(reader)
+
+            current_transcript_counter = 0
+            for line in [l for l in data if l[1] != '-']:
+                current_transcript_counter += 1
+                gene_id = line[TRANSCRIPT_ID_COL_IDX]
+                abundance = line[TRANSCRIPT_ABUNDANCE_COL_IDX]
+                sampled_transcripts_collection[gene_id][sample_name] = abundance
+
+            logging.info('   %s: %i/%i', sample_name, current_transcript_counter, len(data))
+        bar.update(i)
+
+logging.info('# unique transcript_ids: %i', len(sampled_transcripts_collection))
+logging.info('writing the results..')
+
+with open('out.csv', 'wb') as outfile:
     field_names = ['transcript_id'] + sorted(unique_sample_names)
     writer = csv.DictWriter(outfile, fieldnames=field_names)
 
