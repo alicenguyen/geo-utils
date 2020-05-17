5948e81a4e0ea829f762d4ac5f90c8281995eddf
Alice Bollong <amnguyen30@gmail.com>
2020-05-16 19:20:57 -0700

    returns transcript dictionary collection

--- gene-parser.py
+++ gene-parser.py
@@ -1,64 +1,46 @@
 import csv
 import glob
 from collections import defaultdict
+
 import sys
 import tarfile
 
-DATA_FILEPATH='data/GSM3515624_CF10_L004_gene_abund.out.txt'
-# DATA_FILEPATH = 'data/*.csv'
+DATA_FILEPATH = 'sample_data/*.csv'
 TRANSCRIPT_ID_COL_IDX = 0
 TRANSCRIPT_ABUNDANCE_COL_IDX = 8
 
 
-def get_sample_name(string):
-    tokens = string.split('_')
-    return tokens[1] + '_' + tokens[2]
+def extract_sample_name(string):
+    tokens = string.split('_')  # todo improve this
+    return tokens[2] + '_' + tokens[3]
 
 
-def parse_transcript_rows(raw_rows):
-    result = []
-    for row in raw_rows:
-        if row[1] != '-':
-            result.append([row[TRANSCRIPT_ID_COL_IDX], row[TRANSCRIPT_ABUNDANCE_COL_IDX]])
-    return sorted(result)
+overall_set_collection = defaultdict(list)
+sampled_transcripts_collection = defaultdict(dict)
 
+unique_sample_names = []
 
-sampled_transcript_ids = []
-unique_transcript_ids = set()
-unique_samples = set()
+for file_name in glob.glob(DATA_FILEPATH):
+    # if file_name.endswith('tar.gz'):
+    #     tar = tarfile.open(file_name)
+    #     tar.extractall()
+    #     tar.close()
 
-for filename in glob.glob(DATA_FILEPATH):
-    sample_name = get_sample_name(filename)
-    unique_samples.add(sample_name)
+    sample_name = extract_sample_name(file_name)
+    unique_sample_names.append(sample_name)
+
+    print 'starting: ' + sample_name
 
     # read and parse out transcript_ids and abundance values
-    with open(filename) as in_file:
+    with open(file_name) as in_file:
         reader = csv.reader(in_file, delimiter='\t')
-        next(reader, None)  # skip  the headers
+        next(reader, None)  # skip  the header row
 
-        result = []
-        for row1 in reader:
-            transcript_id = row1[TRANSCRIPT_ID_COL_IDX]
-            unique_transcript_ids.add(transcript_id)
+        for line in [l for l in reader if l[1] != '-']:
+            gene_id = line[TRANSCRIPT_ID_COL_IDX]
+            abundance = line[TRANSCRIPT_ABUNDANCE_COL_IDX]
+            sampled_transcripts_collection[gene_id][sample_name] = abundance
 
-            if row[1] != '-':
-                result.append((transcript_id, u))
-
-        for row in sorted(result):
-            transcript_id, abundance = row
-            data = (transcript_id, sample_name, abundance)
-            sampled_transcript_ids.append(data)
-
-print 'unique transcript ids: ', len(unique_transcript_ids)
-print 'unique samples: ', len(unique_samples)
-print 'total sampled transcripts: ', len(sampled_transcript_ids)
-
-for transcript_id in sorted(unique_transcript_ids):
-    transcript_dict = defaultdict(dict)
-    for sample_name in sorted(unique_samples):
-        transcript_dict[transcript_id][sample_name] = None
-        for match in [t for t in sampled_transcript_ids if t[:2] == (transcript_id, sample_name)]:
-            transcript_dict[transcript_id][sample_name] = match[2]
-
-print(sorted(transcript_dict.items()))
+print 'genes count: ', len(sampled_transcripts_collection)
+print 'sample_file count: ', len(unique_sample_names)
 
